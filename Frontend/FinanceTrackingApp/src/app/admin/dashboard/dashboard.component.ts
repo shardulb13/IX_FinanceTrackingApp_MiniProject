a153674bd1ab0f318397c6afc7d8be408a53fd54
shardulb13 <sharbhingardive777@gmail.com>
2022-09-11 12:17:53 +0530

    added owe details to the UI

--- Frontend/FinanceTrackingApp/src/app/admin/dashboard/dashboard.component.ts
+++ Frontend/FinanceTrackingApp/src/app/admin/dashboard/dashboard.component.ts
@@ -11,11 +11,11 @@ export class DashboardComponent implements OnInit {
   allExpense:any=[];
   allUsers: any=[];
   result=0;
+  temp=0;
   oweAmount:number = 0;
   lentAmount:number =0;
-  breakdownAmt:number=0;
   arrayofowe:any=[];
-  paidBy!:any;
+  showOweAmount:any=[];
   
   constructor(private authService: AuthenticationService, private expService: ExpenseService ) {}
 
@@ -28,19 +28,35 @@ export class DashboardComponent implements OnInit {
         this.currentUser = res;
        
       for(let i =0; i<= this.allExpense.length; i++){
-        if(this.allExpense[i].paidBy == this.currentUser.userName){
+        if(this.allExpense[i].paidBy.toLowerCase() == this.currentUser.userName.toLowerCase()){
           this.result += this.allExpense[i].amount;
-        }
-        if(this.allExpense[i].paidBy == this.currentUser.userName){
           console.log("Usernames matched");
-          let temp = (this.oweAmount + this.allExpense[i].amount)/this.allExpense[i].userId.length * (this.allExpense[i].userId.length-1);
-          this.oweAmount = Math.floor(temp);
+          this.temp = (this.allExpense[i].amount)/this.allExpense[i].userId.length ;
+          this.oweAmount += Math.floor(this.temp)* (this.allExpense[i].userId.length-1);
           console.log("Denyache Paise", this.oweAmount);
+          this.arrayofowe = this.allExpense[i].userId;
+          console.log("Mala"+ this.arrayofowe +"Amount"+this.oweAmount);
+          console.log("Array of owe amount", this.arrayofowe);  
+
+          for(let j = 0; j< this.arrayofowe.length; j++){
+            if(this.arrayofowe[j].toLowerCase() == this.currentUser.userName.toLowerCase()){
+              console.log("Found Matching ids in the array");
+            }
+            else{
+              this.showOweAmount.push({'userName':this.arrayofowe[j],'amount':Math.floor(this.temp), 'ExpenseName':this.allExpense[i].expenseName});
+              // this.showOweAmount.push();
+              console.log("Unmatched id", this.arrayofowe[j] + "owes me" + this.temp);
+              console.log("Array madhe kiti ahe", this.showOweAmount);
+              console.log(this.showOweAmount.userName + "owes me" + this.showOweAmount.amount);
+            }
+           }
         }
-        else if(this.allExpense[i].paidBy != this.currentUser.UserName){
+        
+        else{
           let lentTemp = (this.allExpense[i].amount)/ this.allExpense[i].userId.length;
           this.lentAmount += Math.floor(lentTemp);
           console.log("Mala deyache paise", this.lentAmount);
+          
         }
       }
    
@@ -51,7 +67,8 @@ export class DashboardComponent implements OnInit {
     this.allUsers = res;
     console.log("Username",this.allUsers[0].userName.toLowerCase());
     console.log("all user to lower", this.allUsers.userName.toLowerCase());
-  })
+  });
+
 
   }
 
@@ -74,22 +91,14 @@ export class DashboardComponent implements OnInit {
   // }
 
   getpaidbyId(){
-    for(var i=0; i<this.allExpense.length; i++){
-      this.paidBy = this.allExpense[i].paidBy;
-      console.log("Got the paidby id", this.paidBy);
+   for(let i = 0; i< this.arrayofowe.length; i++){
+    if(this.arrayofowe[i] == this.currentUser.id){
+      console.log("Found Matching ids in the array");
     }
+    else{
+      console.log("Unmatched id", this.arrayofowe[i] + "owes me" + this.oweAmount);
+    }
+   }
   }
-  checkname(){
-    for(var i=0; i<this.allExpense.length; i++){
-        if(this.paidBy== this.allExpense[i].userId){
-          let tempbreakdownamt = this.allExpense[i].amount/this.allExpense[i].userId.length;
-          this.breakdownAmt = tempbreakdownamt;
-          this.arrayofowe.push(this.allExpense[i].userName + this.breakdownAmt);
-          console.log("Array madhe store kela", this.arrayofowe);
-          console.log("Mala tula evdhe deyache ahet" + this.allExpense[i].userName + this.breakdownAmt );
-        }
-
-      }
-    }
   
 }
--- Frontend/FinanceTrackingApp/src/core/services/authentication.service.ts
+++ Frontend/FinanceTrackingApp/src/core/services/authentication.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { TokenService } from './token.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,12 @@ export class AuthenticationService {
   baseApiUrl='http://localhost:46079/api/Authentication'
   constructor(private httpClient:HttpClient, private tokenService: TokenService) { }
 
-  RegsiterDetails(data:string){
-    return this.httpClient.post<any>(`${this.baseApiUrl}/register`,data)
+  RegsiterDetails(data:string):Observable<any>{
+    return this.httpClient.post(`${this.baseApiUrl}/register`,data)
  }
 
-  loginDetails(data:string){
-    return this.httpClient.post<any>(`${this.baseApiUrl}/login`,data);
+  loginDetails(data:string):Observable<any>{
+    return this.httpClient.post(`${this.baseApiUrl}/login`,data);
   }
 
   getCurrentUserDetails(){
